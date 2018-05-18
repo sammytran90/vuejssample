7a2b3e01f48e2f683432c573bf22296838f1559c
TrungTA <trung.trananh0907@gmail.com>
2018-05-18 17:50:29 +0700

    change

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ Vue.component('form-component', {
     template: '#form',
     props: ['value'],
     methods: {
-<<<<<<< HEAD
       onInput: function (event) {
         this.$emit('input', event.target.value)
       }
@@ -31,25 +30,3 @@ Vue.component('form-component', {
   new Vue({
     el: '#app'
   });
-=======
-        onInput: function (event) {
-            this.$emit('input', event.target.value)
-        }
-    }
-});
-Vue.component('content-component', {
-    template: '#content',
-    props: ['user']
-})
-Vue.component('container-component', {
-    template: '#container',
-    data: function () {
-        return {
-            user: 'hero'
-        }
-    }
-})
-new Vue({
-    el: '#app'
-})
->>>>>>> 7ab20560bbddeb3fd72822c9c0e164745969e5f2
